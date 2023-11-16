b492f4c8d28e3bd96e8ef4bb22e57e1c1dd163d6
ajones37 <145590688+ajones37@users.noreply.github.com>
2023-11-16 07:27:27 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){	
 
+   function myEventFunction(){
+    alert('Ariana Jones: This is a test');
+  }
   
   var d4 = document.getElementById('div4');
   d4.addEventListener('click', myEventFunction);
   d4.addEventListener('click', function(){
     console.log('I was Clicked!');
-    d4.style.backgroundColor = '#C00';
     d4.innerHTML = '<strong>Goodbye</strong>';
   });
 }
